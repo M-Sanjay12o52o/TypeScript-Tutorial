94ad0f63f2283af75fe206ba31d855b93bf9e4b5
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-18 07:40:33 +0530

    useState and useEffect example

--- lesson13/src/App.tsx
+++ lesson13/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface User {
+  id: number;
+  username: string;
 }
 
-export default App
+function App() {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    // use effect deals with side effects
+    // it's when something changes
+    // here when the users state changes the useEffect will be called into action
+    console.log("mounting...");
+    console.log("Users: ", users);
+
+    return () => console.log("unmounting...");
+  }, [users]);
+
+  return <div className="App"></div>;
+}
+
+export default App;
